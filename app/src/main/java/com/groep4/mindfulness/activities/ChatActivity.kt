c83bf22b6b302d3ab01afdd7337a758d59513ae8
Jolan <taelman.jolan@hotmail.Com>
2018-11-03 03:48:08 +0100

    Poging tot chat

--- app/src/main/java/com/groep4/mindfulness/activities/ChatActivity.kt
+++ app/src/main/java/com/groep4/mindfulness/activities/ChatActivity.kt
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.groep4.mindfulness.R
-import com.groep4.mindfulness.model.ChatBubble
-import android.widget.ArrayAdapter
+import com.groep4.mindfulness.model.Message
 import android.widget.EditText
 import android.widget.ListView
-import android.widget.Toast
-import com.groep4.mindfulness.adapters.MessageAdapter
+import com.firebase.ui.database.FirebaseListAdapter
+import com.firebase.ui.database.FirebaseListOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import android.widget.TextView
 
 
 class ChatActivity : AppCompatActivity() {
@@ -18,38 +20,36 @@ class ChatActivity : AppCompatActivity() {
     private var listView: ListView? = null
     private var btnSend: View? = null
     private var editText: EditText? = null
-    var myMessage = true
-    private var ChatBubbles: MutableList<ChatBubble>? = null
-    private var adapter: ArrayAdapter<ChatBubble>? = null
+    private var messages: MutableList<Message>? = null
+    private var adapter: FirebaseListAdapter<Message>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        ChatBubbles = mutableListOf()
         listView = findViewById<View>(R.id.list_msg) as ListView
         btnSend = findViewById(R.id.btn_chat_send)
-        editText = findViewById(R.id.msg_type) as EditText
-        adapter = MessageAdapter(this, R.layout.left_chat_bubble, ChatBubbles)
-        listView!!.setAdapter(adapter);
+        btnSend!!.setOnClickListener {
+            editText = findViewById<EditText>(R.id.msg_type)
+            FirebaseDatabase.getInstance().getReference().push().setValue(Message(editText.toString(), FirebaseAuth.getInstance().currentUser!!.displayName))
+            editText!!.setText("")
 
+            displayChatMessages()
+        }
+    }
 
-                btnSend!!.setOnClickListener {
-                    if (editText!!.text.toString().trim { it <= ' ' } == "") {
-                        Toast.makeText(this@ChatActivity, "Please input some text...", Toast.LENGTH_SHORT).show()
-                    } else {
-                        //add message to list
-                        val ChatBubble = ChatBubble(editText!!.text.toString(), myMessage)
-                        ChatBubbles!!.add(ChatBubble)
-                        ChatBubbles
-                        adapter!!.notifyDataSetChanged()
-                        editText!!.setText("")
-                        if (myMessage) {
-                            myMessage = false
-                        } else {
-                            myMessage = true
-                        }
-                    }
-                }
+    private fun displayChatMessages() {
+
+        val options: FirebaseListOptions<Message> = FirebaseListOptions.Builder<Message>()
+                .setLayout(R.layout.left_chat_bubble)
+                .setQuery(FirebaseDatabase.getInstance().reference, Message::class.java)
+                .build()
+
+        adapter = object : FirebaseListAdapter<Message>(options) {
+            override fun populateView(v: View, model: Message, position: Int) {
+                val messageText = v.findViewById(R.id.txt_msg) as TextView
+                messageText.text = model.content
+            }}
+        listView!!.adapter = adapter
     }
 }
--- app/src/main/java/com/groep4/mindfulness/model/DataClasses.kt
+++ app/src/main/java/com/groep4/mindfulness/model/DataClasses.kt
@@ -2,4 +2,3 @@ package com.groep4.mindfulness.model
 
 data class Sessie(val naam: String, val beschrijving: String, val oefeningen: ArrayList<Oefening>, val vergrendeld: Boolean)
 data class Oefening(val naam: String, val beschrijving: String)
-data class Message(val titel: String, val onderwerp: String, val afzender: String, val  tekst : String)
