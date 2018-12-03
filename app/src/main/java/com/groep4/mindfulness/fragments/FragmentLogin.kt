17bff1420590debd4809121fcd7988dcd1e2538c
simonanckaert <simon.anckaert.y0223@student.hogent.be>
2018-12-03 11:55:51 +0100

    Google login werkt

--- app/src/main/java/com/groep4/mindfulness/fragments/FragmentLogin.kt
+++ app/src/main/java/com/groep4/mindfulness/fragments/FragmentLogin.kt
@@ -9,13 +9,23 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.annotation.TargetApi
 import android.content.Intent
+import android.support.constraint.Constraints.TAG
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.groep4.mindfulness.R
+import com.groep4.mindfulness.activities.ActivityPage
 import com.groep4.mindfulness.activities.MainActivity
 import com.groep4.mindfulness.utils.LoginValidation
 
@@ -23,13 +33,23 @@ import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.fragment_login.view.*
 
 
-class FragmentLogin : Fragment(){
+ class FragmentLogin : Fragment(){
 
     /**
      * Houd de inlogtaak bij om ervoor te zorgen dat we deze kunnen annuleren als hierom wordt gevraagd.
      */
 
+    private val RC_SIGN_IN: Int = 1
     lateinit var mAuth: FirebaseAuth
+    lateinit var gso : GoogleSignInOptions
+
+     override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+         gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                 .requestIdToken(getString(R.string.default_web_client_id))
+                 .requestEmail()
+                 .build()
+     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -156,10 +176,55 @@ class FragmentLogin : Fragment(){
         }
     }
 
-    private fun attemptGoogleLogin(){
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                Toast.makeText(activity!!.applicationContext, e.toString(), Toast.LENGTH_SHORT)
+                // ...
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(activity!!) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success")
+                        val user = mAuth.currentUser
+                        val intent = Intent(activity, MainActivity::class.java)
+                        this.startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                        Toast.makeText((activity!!).applicationContext, "Authentication Failed.", Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+
+                    // ...
+                }
+    }
+
+    fun attemptGoogleLogin(){
 
         //ToDo: Google login
-
+        val googleSignInClient = GoogleSignIn.getClient(activity!!, gso!!)
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
 
