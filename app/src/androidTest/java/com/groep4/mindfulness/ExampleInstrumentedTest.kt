39343c53a0796c7600358ec6e99694736e94eae4
simonanckaert <simon.anckaert@sgsintpaulus.eu>
2019-08-19 10:50:02 +0200

    Testen geschreven

--- app/src/androidTest/java/com/groep4/mindfulness/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/groep4/mindfulness/ExampleInstrumentedTest.kt
@@ -1,12 +1,12 @@
 package com.groep4.mindfulness
 
-import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import com.groep4.mindfulness.utils.LoginValidation
+import org.junit.Assert
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +15,46 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.sdrz.mindfulness_00", appContext.packageName)
+    fun isGeldigMailadres_MailadresBevatGeenAt_GeeftFalseTerug() {
+        var gelukt = LoginValidation.isValidEmail("examplehotmail.com", false)
+        Assert.assertFalse(gelukt)
+    }
+
+    @Test
+    fun isGeldigMailadres_MailadresBevatGeenTekstNaAt_GeeftFalseTerug() {
+        var gelukt = LoginValidation.isValidEmail("example@", false)
+        Assert.assertFalse(gelukt)
+    }
+
+    @Test
+    fun isGeldigMailadres_MailadresGeldig_GeeftTrueTerug() {
+        var gelukt = LoginValidation.isValidEmail("example@hotmail.com", false)
+        Assert.assertTrue(gelukt)
+    }
+
+    @Test
+    fun isGeldidWachtwoord_WachtwoordZonderHoofdletter_GeeftFalseTerug() {
+        var gelukt = LoginValidation.isValidPassword("example123", false)
+        Assert.assertFalse(gelukt)
+    }
+
+    @Test
+    fun isGeldigWachtwoord_WachtwoordZonderCijfers_GeeftFalseTerug() {
+        var gelukt = LoginValidation.isValidPassword("Exampleeee", false)
+        Assert.assertFalse(gelukt)
+    }
+
+    @Test
+    fun isGeldigWachtwoord_WachtwoordKorterDanAchtTekens() {
+        var gelukt = LoginValidation.isValidPassword("Ex1", false)
+        Assert.assertFalse(gelukt)
+    }
+
+    @Test
+    fun isGeldigWachtwoord_WachtwoordGeldig_GeeftTrueTerug() {
+        var gelukt = LoginValidation.isValidPassword("Example123", false)
+        Assert.assertTrue(gelukt)
     }
 }
